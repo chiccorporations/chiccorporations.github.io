1f6efafbe045bcbd444e40106ac2728ae0ed9bcf
Lkfunkyo X <Lkfunkyo X>
2016-06-29 00:17:24 -0400

    index.js updated

--- js/index.js
+++ js/index.js
@@ -1,7 +1,24 @@
 function setup(){
-  noCanvas();
+  createCanvas(displayWidth, displayHeight-105);
+  
+  for(var i = 0; i < 2000; i++){
+    fill(255, random(50, 80));
+    strokeWeight(5);
+    stroke(255, 100);
+    drawRect();
+  }
 }
 
 function draw(){
   
+}
+
+function drawRect(){
+  push();
+  
+  rotate(radians(random(360)));
+  translate(random(width), random(height));
+  scale(0.75);
+  rect(0, 0, random(30, 50), random(30, 50));
+  pop();
 }
