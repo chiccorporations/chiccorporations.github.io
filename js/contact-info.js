f8c4a6d478cd38d5e53dde9152a2f067fb4a5797
Lkfunkyo X <Lkfunkyo X>
2016-08-08 10:24:32 -0400

    restyled

--- js/contact-info.js
+++ js/contact-info.js
@@ -1,41 +1,30 @@
-var name, email, comment;
-var inputs;
-var message;
-var button;
+var email, firstName, lastName, description, textArea;
+var emailLabel, firstNameLabel, lastNameLabel;
 
-function setup() {
-  noCanvas();
-
-  name = createInput();
+function setup(){
+  firstNameLabel = createElement('label', 'First Name:\t');
+  firstNameLabel.attribute('for', 'firstName')
+  firstName = createInput();
+  
+  lastNameLabel = createElement('label', 'Last Name:\t');
+  lastNameLabel.attribute('for', 'lastName')
+  lastName = createInput();
+  
+  emailLabel = createElement('label', 'Email:\t');
+  emailLabel.attribute('for', 'email');
   email = createInput();
-  comment = createInput();
-  button = createButton('click');
   
-  inputs = document.getElementsByTagName('input');
-}
-
-function mouseClicked() {
-  button.mouseClicked(emailInput);
-}
-
-function emailInput() {
-  var counter = 0;
-  message = ' ';
-  for (var i = 0; i < inputs.length; i++) {
-    if (inputs[i].value.length > 2) {
-      message += inputs[i].value;
-      message += '\n';
-
-      counter++;
-    }
-  }
-
-
-  if (counter == inputs.length) {
-    console.log(message);
-  } else {
-    alert('Not all the criteria were filled out. You must finish in order to ensure the creation of your designs!');
-  }
+  textArea = createElement('textarea');
   
+  email.attribute('name', 'email');
+  email.attribute('placeholder', 'eg: pillsburydoeboi43@gmail.com');
   
+  firstName.attribute('name', 'email');
+  firstName.attribute('placeholder', 'eg: John');
+  
+  lastName.attribute('name', 'email');
+  lastName.attribute('placeholder', 'eg: Doe');
+  
+  textArea.attribute('rows', '4');
+  textArea.attribute('cols', '50');
 }
