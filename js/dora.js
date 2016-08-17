7b7f14439fcd9b377c615717c6e9e67ee5e56277
Lkfunkyo X <Lkfunkyo X>
2016-08-17 19:26:08 -0400

    scaling fixed

--- js/dora.js
+++ js/dora.js
@@ -1,27 +1,8 @@
 function setup(){
-  noCanvas();
-  
-  scene.heading = createElement('h1', "YOU'VE WON!!!");
-  scene.heading.attribute('display', 'block');
-  scene.heading.attribute('color', 'rgb(0, 200, 200)');
-  scene.heading.attribute('margin-right', 'auto');
+  createCanvas(displayWidth, displayHeight);
+  background(255);
 }
 
 function draw(){
-  scene.run();
-}
-
-var scene = {
-  heading: 0,
-  counter: 0,
   
-  run: function(){
-    this.counter += this.rt;
-    
-    if(this.counter > 20){
-      this.rt *= -1;
-    }
-    
-    //this.heading.attribute('color', 'rgb(' + map(this.counter, 0, 20, 0, 255) + ')');
-  },
-};
+}
